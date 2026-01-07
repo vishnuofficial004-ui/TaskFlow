780255ad4b3fc7c26d3668f24bc47c51821a4ea8
vishnuofficial004-ui <vishnuofficial004@gmail.com>
2026-01-07 15:53:13 +0530

    frontend-setup

---
+++ frontend/src/App.js
@@ -0,0 +1,31 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import { getToken } from "./utils/auth";
+
+const ProtectedRoute = ({ children }) => {
+  return getToken() ? children : <Navigate to="/login" />;
+};
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="*" element={<Navigate to="/login" />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
